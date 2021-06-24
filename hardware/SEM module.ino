41e2d90b86a3976a1e8f250c03222831cb94d878
Skbhai <saurabhkgr18@gmail.com>
2021-06-24 20:58:04 +0530

    Hardware module updated

--- hardware/SEM module.ino
+++ hardware/SEM module.ino
@@ -1,24 +1,24 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
-
+ 
 HTTPClient http;
-
-const char* ssid     = "Redmi Note 8";
-const char* password = "BAF750A7";
+ 
+const char* ssid     = "D7Y30R32";
+const char* password = "123456789";
 bool reconnecting = false;
 String url = "http://smartenergymeter1234.herokuapp.com/meter/";
 String m_id = "4";    // put your meter id here 
 unsigned long StartTime = millis();
 int pulses = 0;
-
+ 
 void setup() {
-
-  Serial.begin(115200);
+ 
+  Serial.begin(9600);
   Serial.flush();
   Serial.println();
   Serial.println("This is Smart Energy Meter Module");
-
+ 
   WiFi.mode(WIFI_STA); 
   if(setup_WiFi()){
     Serial.println("WiFi connected");
@@ -30,18 +30,22 @@ void setup() {
   // allow connection reuse (if server supports it)
   http.setReuse(true);
 }
-
+ 
 void loop() {
-
-  pulses += random(1, 10);  //analogRead(LDR);
+  int ldrv=analogRead(A0);
+  if(ldrv>12) pulses+=1;
+  
+ 
+//Serial.println(ldrv);
   //elapsed time 
   unsigned long CurrentTime = millis();
   unsigned long ElapsedTime = CurrentTime - StartTime;
-  Serial.println(ElapsedTime);
+  //Serial.println(ElapsedTime);
   if(ElapsedTime > 5000){
       // check for WiFi connection
       if ((WiFi.status() == WL_CONNECTED)) {
         WiFiClient client;
+        Serial.println(pulses);
         http.begin(client, poll(m_id, String(pulses)));      //poll(m_id, pulse)
         int httpResponseCode = http.GET();
         if (httpResponseCode > 0) {
@@ -70,12 +74,12 @@ void loop() {
 //  delay(60000);
   }
 }
-
+ 
 // function  to setup wifi and handle reconnection
 bool setup_WiFi(){
   if(reconnecting)Serial.print("Reconnecting : ");
   else Serial.print("Connecting : ");
-
+ 
   Serial.println(ssid);
   
   WiFi.begin(ssid, password);
@@ -88,7 +92,7 @@ bool setup_WiFi(){
   
   return true;
 }
-
+ 
 // helper function to get complete url
 String poll(String m_id, String pulse){
   return url +m_id +"/"+pulse+"/";
